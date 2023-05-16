6fbca2399064305e226106bbc19dfc3be580436d
Jayesh Patil <patiljayesh1421999@gmail.com>
2023-05-16 17:53:47 +0530

    Add project files.

---
+++ PocSiganalR/Hubs/MoveViewHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PocSiganalR.Hubs
+{
+    public class MoveViewHub:Hub
+    {
+        public async Task MoveView(string name, string amount)
+        {
+            await Clients.Others.SendAsync("PU9vgKcfYm1ozi9FRtao", "You have received "+amount+" Rs from "+name+" on Merchant App");
+        }
+    }
+}
